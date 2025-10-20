3b0a5c245253701a55d77b2f81af3239e09666c8
Orlando Caesar Donesa <orlandocaesardonesa@gmail.com>
2025-10-21 07:15:58 +0800

    Created AccessLevel and FunctionModel models

---
+++ app/Models/AccessLevel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\FunctionModel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AccessLevel extends Model
+{
+    /** @use HasFactory<\Database\Factories\AccessLevelFactory> */
+    use HasFactory;
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'access_levels';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Relationship: AccessLevel ↔ FunctionModel (Many-to-Many)
+     */
+    public function functions()
+    {
+        return $this->belongsToMany(FunctionModel::class, 'access_functions', 'access_id', 'function_id');
+    }
+
+    /**
+     * Relationship: AccessLevel → Users (One-to-Many)
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class, 'access_id');
+    }
+}
---
+++ app/Models/FunctionModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class FunctionModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'functions';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Relationship: FunctionModel ↔ AccessLevel (Many-to-Many)
+     */
+    public function accessLevels()
+    {
+        return $this->belongsToMany(AccessLevel::class, 'access_functions', 'function_id', 'access_id');
+    }
+}
